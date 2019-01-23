85c5ed9c195eaf573bd76eda5642ed5f7802120c
Shresthaoshen <41971739+Shresthaoshen@users.noreply.github.com>
2019-01-23 10:24:07 -0500

    Add files via upload

---
+++ Setup.java
@@ -0,0 +1,39 @@
+package shresthao;
+
+import bridges.connect.Bridges;
+import bridges.base.Array;
+import bridges.base.Element;
+
+public class Setup {
+	public static void main(String[] args) throws Exception {
+
+	    /* Initialize a Bridges connection with your credentials */
+	    /* TODO: plug your own BRIDGES credentials */
+	    Bridges bridges = new Bridges(0, "shreshtao", "392719838445");
+
+	    /* Set an assignment title */
+	    bridges.setTitle("Oshen Shrestha");
+
+	    /* Set up the array dimensions, allocate an Array of Elements */
+	    /* TODO: Make an array of size 10 */
+	    int arraySize = 10;
+	    Array<Integer> arr = new Array<Integer> (arraySize);
+	    
+	    /* Populate the array with integers */
+	    /* TODO: Make the array store square numbers*/
+	    
+	    for(int i = 0; i < arraySize; i++) {
+	    	arr.getElement(i).setValue (i*i);
+	    	 arr.getElement(i).setLabel(String.valueOf(arr.getElement(i).getValue()));
+	    }
+	    
+	    /* set the value as a Label */
+	   
+	    
+	    /* Tell BRIDGES which data structure to visualize */
+	    bridges.setDataStructure(arr);
+
+	    /* Visualize the Array */
+	    bridges.visualize();
+	}
+}
